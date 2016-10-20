0ccbe67716f679b5aa179ab3d727ec430f190b21
Evgeny <turusov_evg@mail.ru>
2016-10-20 17:35:18 +0500

    Добавил уничтожение человека, при встрече с игроком

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+  void OnTriggerEnter2D(Collider2D otherCollider)
+  {
+    Man man = otherCollider.gameObject.GetComponent<Man>();
+    if (man != null)
+    {
+        Destroy(man.gameObject);
+      }
+    }
+  }
