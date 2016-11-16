6ca13157f5ef53161414c6ff4bbda2a26ac1326e
Evgeny <turusov_evg@mail.ru>
2016-11-16 19:53:14 +0500

    Hp для монстра и ракета

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+  public float hp = 1;
+
+  public float CountDamage = 0.1f;
+
+  public void Damage(float damageCount)
+  {
+    hp -= damageCount;
+    if (hp <= 0)
+    {
+      Destroy(gameObject);
+    }
+  }
+
+  void OnTriggerEnter2D(Collider2D otherCollider)
+  {
+    Rocket shot = otherCollider.gameObject.GetComponent<Rocket>();
+    if (shot != null)
+    {
+        Damage(CountDamage);
+        Destroy(shot.gameObject);
+    }
+  }
+}
