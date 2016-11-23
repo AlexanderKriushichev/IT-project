614fb0bf3ec99d026092dd404b60e711445dcb67
ArturRush <arturrush@rambler.ru>
2016-11-23 11:59:36 +0500

    Запилил выход и перезагрузку при проигрыше

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+	public void ReplayGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
